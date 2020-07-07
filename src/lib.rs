c6f2cdc6e14ef71eaa21aa32e5bec610b64f1654
Patrick Lühne <patrick@luehne.de>
2020-07-07 08:38:28 +0200

    Improve public interface

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,11 @@
 mod ast;
 pub mod flavor;
-pub mod format;
-pub mod parse;
+mod format;
+mod parse;
 mod utils;
 
 pub use ast::*;
+pub use format::{formulas::FormulaDisplay, terms::TermDisplay};
+pub use flavor::{DefaultFlavor, Flavor};
 pub use utils::*;
+pub use parse::{DefaultParser, Parser};
